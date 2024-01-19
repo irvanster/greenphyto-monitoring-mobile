6e4c55113afd4c58a84d66733a542cf50f280af1
irvanster <tovamaulana098@gmail.com>
2024-01-20 04:48:50 +0700

    ignore pr

--- lib/screens/detail.dart
+++ lib/screens/detail.dart
@@ -12,6 +12,7 @@ class SecondRoute extends StatefulWidget {
   const SecondRoute({Key? key, required this.locationId}) : super(key: key);
 
   @override
+  // ignore: library_private_types_in_public_api
   _SecondRouteState createState() => _SecondRouteState();
 }
 
